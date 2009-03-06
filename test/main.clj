646a6c2a4c1d8f411b2a21c5f162b9d516911a5c
Brian Doyle <brianpdoyle@gmail.com>
2009-03-05 22:49:18 -0700

    added more tests for redirect and cookies

--- test/main.clj
+++ test/main.clj
@@ -4,11 +4,12 @@
 
 (def clj-ws (wc/client "http://www.clojure.org"))
 (def home (wc/method "/"))
+(def clj-home-page-text "Clojure is a dialect of Lisp")
 
 (deftest client 
   (let [host "www.clojure.org"
-        clj-ws (wc/client (str "http://" host))]
-    (is (= (.. clj-ws (getHostConfiguration) getHost) host))))
+        ws (wc/client (str "http://" host))]
+    (is (= (.. ws (getHostConfiguration) getHost) host))))
 
 (deftest method
    (let [default-get-method (wc/method "/")
@@ -26,6 +27,23 @@
 
 (deftest scrape
   (let [html (wc/scrape clj-ws home)]
-    (is (= (.contains html "Clojure")))))
+    (is (= (.contains html clj-home-page-text)))))
 
+; this test depends on a website that i don't have any control over,  
+; this test is fragile, but better than no test
+(deftest cookie-names
+  (wc/send-method clj-ws home)
+  ;(println "Cookies from clojure.org ") 
+  ;(wc/print-cookies clj-ws)
+  (is (wc/assert-cookie-names clj-ws "test" "master"))
+  (is (not (wc/assert-cookie-names clj-ws "does-not-exist-at-all"))))
+
+; this test depends on a website that i don't have any control over,  
+; this test is fragile, but better than no test
+(deftest redirect
+  ; i setup this redirect at shorturl.com
+  (let [redirect-site (wc/client "http://alturl.com/") 
+        home-page (wc/method "/yew")] 
+    (is (= (.contains (wc/scrape redirect-site home-page) clj-home-page-text))))) 
+   
 (run-tests)
